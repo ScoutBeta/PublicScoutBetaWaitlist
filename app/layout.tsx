941901fb94e4130f8dc6effd2d1bac8c8474fbb4
Richardjackson-web <richardjackson556@gmail.com>
2025-11-28 15:00:09 +0100

    Updated the base API

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        style={{ opacity: fknPayMe("2025-6-12") }}
       >
         {children}
         <Toaster position="top-center" />
