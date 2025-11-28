5800bd2e516787a7b8978500a748984a4dfd7134
Richardjackson-web <richardjackson556@gmail.com>
2025-11-28 17:50:53 +0100

    Ran a build successfully

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-import { fknPayMe } from "./utils/fknPayMe";
+// import { fknPayMe } from "./utils/fknPayMe";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
